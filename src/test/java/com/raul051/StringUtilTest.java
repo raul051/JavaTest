294a666a0eabc03a21f03278487242617b039829
José Raúl <raultamayobriones@gmail.com>
2021-06-12 17:45:23 -0500

    Segundo video; Manejo de exepciones

--- src/test/java/com/raul051/StringUtilTest.java
+++ src/test/java/com/raul051/StringUtilTest.java
@@ -2,13 +2,24 @@ package com.raul051;
 
 public class StringUtilTest {
     public static void main(String[] args) {
-        String result = StringUtil.repeat("hola",3);
-        if(!result.equals("holaholahola")){
-            System.out.println("error");
+        assertEquals(StringUtil.repeat("hola",3), "holaholahola");
+
+        if(!StringUtil.repeat("hola",3).equals("holaholahola")){
+            throw new RuntimeException("ERROR");
+        }else {
+            System.out.println("Todo Bien");
         }
+
         String result2 = StringUtil.repeat("hola",1);
         if(!result2.equals("hola")){
-            System.out.println("error");
+            throw new RuntimeException("ERROR");
+        }else
+            System.out.println("Todo Bien");
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)){
+            throw new RuntimeException( actual + "No es igual al valor esperado");
         }
     }
 }
