cc8a476fce0b81d2b8b105c5083ecf83aec55de1
José Raúl <raultamayobriones@gmail.com>
2021-06-12 22:58:33 -0500

    7mo Video y ultimo de la carpeta 2, Refactor del codigo, notación Before

--- src/test/java/com/raul051/payments/paymentProcesorTest.java
+++ src/test/java/com/raul051/payments/paymentProcesorTest.java
@@ -1,5 +1,6 @@
 package com.raul051.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,22 +8,31 @@ import static org.junit.Assert.*;
 
 public class paymentProcesorTest {
 
+    private PaymentGatway paymentGatway;
+    private paymentProcesor paymentProcesor;
+
+    @Before
+    public void setup(){
+        paymentGatway = Mockito.mock(PaymentGatway.class);
+        paymentProcesor = new paymentProcesor(paymentGatway);
+    }
+
     @Test
     public void payment_is_correct(){
-        PaymentGatway paymentGatway = Mockito.mock(PaymentGatway.class);
-        Mockito.when(paymentGatway.requestPayment(Mockito.any())).thenReturn( new PaymemtResponse(PaymemtResponse.PaymentStatus.OK));
 
-        paymentProcesor paymentProcesor = new paymentProcesor(paymentGatway);
+        Mockito.when(paymentGatway.requestPayment(Mockito.any()))
+                .thenReturn( new PaymemtResponse(PaymemtResponse.PaymentStatus.OK));
+
 
        assertTrue(paymentProcesor.makePayment(1000));
     }
 
     @Test
     public void payment_is_Incorrect(){
-        PaymentGatway paymentGatway = Mockito.mock(PaymentGatway.class);
+
         Mockito.when(paymentGatway.requestPayment(Mockito.any())).thenReturn( new PaymemtResponse(PaymemtResponse.PaymentStatus.ERROR));
 
-        paymentProcesor paymentProcesor = new paymentProcesor(paymentGatway);
+
 
         assertFalse(paymentProcesor.makePayment(1000));
     }
