f9b19270dd9e55716b1ed8d73cdf61762fb26254
José Raúl <raultamayobriones@gmail.com>
2021-06-12 19:01:55 -0500

    Tercer video

--- src/test/java/com/raul051/StringUtilTest.java
+++ src/test/java/com/raul051/StringUtilTest.java
@@ -1,25 +1,14 @@
 package com.raul051;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
-        assertEquals(StringUtil.repeat("hola",3), "holaholahola");
+    @Test
+    public static void testRepeat(){
 
-        if(!StringUtil.repeat("hola",3).equals("holaholahola")){
-            throw new RuntimeException("ERROR");
-        }else {
-            System.out.println("Todo Bien");
-        }
-
-        String result2 = StringUtil.repeat("hola",1);
-        if(!result2.equals("hola")){
-            throw new RuntimeException("ERROR");
-        }else
-            System.out.println("Todo Bien");
+        Assert.assertEquals(StringUtil.repeat("hola",3), "holaholahola");
+        Assert.assertEquals(StringUtil.repeat("hola",1),"hola");
     }
 
-    private static void assertEquals(String actual, String expected) {
-        if(!actual.equals(expected)){
-            throw new RuntimeException( actual + "No es igual al valor esperado");
-        }
-    }
 }
